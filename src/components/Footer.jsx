bfe6219076d154302a62335bd59a6ad6a408a6be
Priyanshu Srivastava <priyanshusrivastava770@gmail.com>
2024-05-11 23:52:30 +0530

    Baat-Chit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/Footer.css";
+
+function Footer() {
+  const handleClick = function () {
+    window.open("https://priyanshuportfolio009.netlify.app/", "_blank", "noopener");
+  };
+
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p>
+          Thodi Baat Chit with <i className="f__text f__heart__symbol">&#10084;</i>{" "}
+          {" "}
+          <span className="f__text dev__name nowrap" onClick={handleClick}>
+            Priyanshu Srivastva
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
